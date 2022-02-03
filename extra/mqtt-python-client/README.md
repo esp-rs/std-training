b94296c3a22efbf5617cb5deedf86de44c0d39d2
Anatol Ulrich <anatol@versteht.es>
2022-02-03 23:44:38 +0100

    fix python client + dox

--- extra/mqtt-python-client/README.md
+++ extra/mqtt-python-client/README.md
@@ -4,12 +4,13 @@
 - logs temperature values sent by MCU
 
 ## Setup
-- Configure MQTT credentials in `../../../intro/mqtt/exercise` according to instructions provided in the workshop 
+- Configure MQTT credentials in `../../intro/mqtt/exercise` according to instructions provided in the workshop 
 - set up a Python environment:
 
 ```shell
 # setup (once):
 $ python3 -m venv ve
+$ source ./ve/bin/activate
 $ pip3 install paho-mqtt toml
 
 # setup (every time you open a new shell):
--- extra/mqtt-python-client/mqtt.py
+++ extra/mqtt-python-client/mqtt.py
@@ -11,17 +11,17 @@ uuid = None
 
 
 def get_uuid():
-    d = toml.load(open("../../lib/get-uuid/uuid.toml", "r"))
-    return list(d.values())[0]['uuid']
+    d = toml.load(open("../../common/lib/get-uuid/uuid.toml", "r"))
+    return list(d.values())[0]["uuid"]
 
 
 def get_config():
-    d = toml.load(open("../../../intro/mqtt/exercise/cfg.toml", "r"))
+    d = toml.load(open("../../intro/mqtt/exercise/cfg.toml", "r"))
     return list(d.values())[0]
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
     data_topic = f"{uuid}/sensor_data/#"
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -33,13 +33,13 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    [mcu_temp] = unpack('>f', msg.payload)
+    [mcu_temp] = unpack(">f", msg.payload)
     print(f"{msg.topic} {mcu_temp:.1f}")
 
 
 def msg():
     color = [randint(0, 255), randint(0, 255), randint(0, 255)]
-    return f"{uuid}/cmd/led", bytearray(color)
+    return f"{uuid}/command/board_led", bytearray(color)
 
 
 def connect():
@@ -47,18 +47,18 @@ def connect():
     uuid = get_uuid()
     config = get_config()
     client = mqtt.Client()
-    client.username_pw_set(config['mqtt_user'], config['mqtt_pass'])
+    client.username_pw_set(config["mqtt_user"], config["mqtt_pass"])
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(config["mqtt_host"], 1883, 60)
     client.loop_start()
-    garbage = "1234567890abcdef01234567890abcdef" * 190 + 'ZZZ'
+    garbage = "1234567890abcdef01234567890abcdef" * 190 + "ZZZ"
     while True:
-        #client.publish(topic, data)
+        # client.publish(topic, data)
         cmd_topic, data = msg()
         print(f"set color {list(data)}")
         client.publish(cmd_topic, data)
-        client.publish(f"{config['uuid']}/cmd/garbage", garbage)
+        client.publish(f"{uuid}/cmd/garbage", garbage)
         sleep(1)
 
 
