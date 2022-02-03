76884ff03749f53d757747b03e3f183cda7e5d4a
Anatol Ulrich <anatol@versteht.es>
2022-02-03 17:10:49 +0100

    remove config.rs

--- intro/hardware-check/src/main.rs
+++ intro/hardware-check/src/main.rs
@@ -4,13 +4,18 @@ use bsc::{
     led::{RGB8, WS2812RMT},
     wifi,
 };
-use config::CONFIG;
 use esp32_c3_dkc02_bsc as bsc;
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_sys as _;
 use log::info;
 
-mod config;
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
--- intro/http-server/solution/src/config.rs
+++
@@ -1,7 +0,0 @@
-#[toml_cfg::toml_config]
-pub struct Config {
-    #[default("")]
-    wifi_ssid: &'static str,
-    #[default("")]
-    wifi_psk: &'static str,
-}
