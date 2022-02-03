7a9671cc15c4e2b15885968188a3a2245e235305
Anatol Ulrich <anatol@versteht.es>
2022-02-03 17:05:21 +0100

    http server exercise

---
+++ intro/http-server/exercise/build.rs
@@ -0,0 +1,6 @@
+fn main() -> anyhow::Result<()> {
+    // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
+
+    embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
+    embuild::build::LinkArgs::output_propagated("ESP_IDF")
+}
---
+++ intro/http-server/exercise/src/config.rs
@@ -0,0 +1,7 @@
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
