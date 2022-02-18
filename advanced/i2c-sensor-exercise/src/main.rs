75c779f9e46332cd684bd857a12b49bdacf5fa42
Mirabellensaft <tanks@ferrous-systems.com>
2022-02-18 16:39:20 +0100

    i2c code

--- advanced/i2c-sensor-exercise/src/main.rs
+++ advanced/i2c-sensor-exercise/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
-use embedded_hal::blocking::delay::DelayMs;
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
+use esp_idf_hal::ledc::{config::TimerConfig, Channel, Timer};
 use esp_idf_hal::{
     delay::FreeRtos,
     i2c::{config::MasterConfig, Master, MasterPins, I2C0},
@@ -7,40 +8,78 @@ use esp_idf_hal::{
     prelude::*,
 };
 use esp_idf_sys::*;
-use lis3dh::{accelerometer::Accelerometer, Lis3dh, SlaveAddr};
+use i2c_sensor_exercise::imc42670p::{IMC42670P, SlaveAddr};
 
-fn main() -> Result<()> {
+use shtcx::{self, PowerMode, MeasurementDuration};
+
+use shared_bus;
+
+fn main() -> anyhow::Result<()>  {
     link_patches();
 
     let peripherals = Peripherals::take().unwrap();
 
-    let sda = peripherals.pins.gpio0;
-    let scl = peripherals.pins.gpio1;
+    let config = TimerConfig::default().frequency(25.kHz().into());
+
+    let sda = peripherals.pins.gpio4;
+    let scl = peripherals.pins.gpio5;
 
     let i2c = Master::<I2C0, _, _>::new(
         peripherals.i2c0,
         MasterPins { sda, scl },
         <MasterConfig as Default>::default().baudrate(400.kHz().into()),
     )?;
-    let mut lis3dh = Lis3dh::new_i2c(i2c, SlaveAddr::Default).unwrap();
-    lis3dh.enable_temp(true).unwrap();
+
+    let bus = shared_bus::BusManagerSimple::new(i2c);
+
+    let proxy_1 =bus.acquire_i2c();
+    let proxy_2 =bus.acquire_i2c();
+
+    let mut imu = IMC42670P::new(proxy_1, SlaveAddr::B110_1001)?;
+    println!("Sensor init");
+    let device_id = imu.read_device_id_register()?;
+    println!("Device ID: {}", device_id);
+    let conf = imu.read_configuration()?;
+    println!("configuration: {}", conf.bits);
+
+    imu.gyro_ln();
+    println!("Gyro on");
+
+    let conf = imu.read_configuration()?;
+    println!("configuration: {}", conf.bits);
+
+
+
+
+
+    let mut sht = shtcx::shtc3(proxy_2);
+    let device_id = sht.device_identifier().unwrap();
+ 
+
+    println!("Device ID: {}", device_id);
+
+    
+    
 
     loop {
-        let accel = lis3dh.accel_norm().unwrap();
-        let temp = lis3dh.get_temp_outf().unwrap();
-        let hum = 
+        let gyro_data =imu.read_gyro()?;
+        sht.start_measurement(PowerMode::NormalMode).unwrap();
+        FreeRtos.delay_ms(100u32);
+        let measurement = sht.get_measurement_result().unwrap(); 
+        
 
         println!(
-            "ACCEL: X: {:.4}  Y: {:.4}  Z: {:.4}\n
+            " GYRO: X: {:.4}  Y: {:.4}  Z: {:.4}\n
             TEMP: {}\n
-            HUM: {}\n
+            HUM: {:?}\n
             \n 
-            "
+            ",
 
-            accel.x, accel.y, accel.y, temp, hum
+            gyro_data.x, gyro_data.y, gyro_data.y, 
+            measurement.temperature.as_degrees_celsius(), measurement.humidity.as_percent(),
         );
 
-        FreeRtos.delay_ms(100u32);
+        FreeRtos.delay_ms(500u32);
     }
 }
 
