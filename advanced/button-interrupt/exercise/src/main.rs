c00bb7c5cc141c6e098801bd26faea0e7388d2fb
Mirabellensaft <tanks@ferrous-systems.com>
2022-02-24 13:13:45 +0100

    change folder strukture

--- advanced/button-interrupt/exercise/src/main.rs
+++ advanced/button-interrupt/exercise/src/main.rs
@@ -9,12 +9,11 @@ use esp_idf_sys::{
     gpio_int_type_t_GPIO_INTR_POSEDGE, gpio_isr_handler_add, gpio_mode_t_GPIO_MODE_INPUT,
 };
 
+// 5. Define a static mute that holds the status of the interrupt handler
 static mut ISR_TX: Option<mpsc::Sender<()>> = None;
 
-// TODO place code in ram using IRAM linker feature
-// https://docs.espressif.com/projects/esp-idf/en/latest/esp32c3/api-guides/linker-script-generation.html
-// uncommenting this "works", TODO test/verify
-#[link_section = ".iram0.text"]
+
+// 6. define what the interrupt handler does, once the button is pushed. button_interrupt sends a message (of type ()) over the tx ("transmitter") part of the channel. 
 unsafe extern "C" fn button_interrupt(_: *mut c_void) {
     ISR_TX.as_mut().unwrap().send(());
 }
@@ -22,28 +21,39 @@ unsafe extern "C" fn button_interrupt(_: *mut c_void) {
 fn main() -> anyhow::Result<()> {
     const GPIO_NUM: i32 = 9;
 
-    let io_conf = gpio_config_t {
-        pin_bit_mask: 1 << GPIO_NUM,
-        mode: gpio_mode_t_GPIO_MODE_INPUT,
-        pull_up_en: true.into(),
-        pull_down_en: false.into(),
-        intr_type: gpio_int_type_t_GPIO_INTR_POSEDGE, // positive edge trigger = button down
-    };
+    // 1. Add GPIO configuration c struct
+    // let io_conf = gpio_config_t {
+    //     ...
+    // };
 
-    let (tx, rx) = mpsc::channel::<()>();
+    // 4. Add the simple streaming channel for messages of the type ()
+    // let (tx, rx) = ...
 
+    // 
     unsafe {
-        ISR_TX = Some(tx);
-        esp!(gpio_config(&io_conf))?;
+
+        // 2. write the GPIO configuration into the register
+        // esp!(...)?;
+
+        // Flag used to allocate the interrupt
         const ESP_INTR_FLAG_IRAM: i32 = 1 << 10;
-        esp!(gpio_install_isr_service(ESP_INTR_FLAG_IRAM))?;
+
+        // 3. Install the global GPIO interrupt handler
+        // esp!(...)?;
+        
+        
+        // 7. Add our GPIO to the interrupt handler
         esp!(gpio_isr_handler_add(
             GPIO_NUM,
             Some(button_interrupt),
             std::ptr::null_mut()
         ))?;
+
+        // 8. Moves tx into the interrupt handler
+        ISR_TX = Some(tx);
     }
 
+    // the loop in main waits until it gets a message through the rx ("receiver") part of the channel
     loop {
         match rx.recv() {
             Ok(_) => println!("button pressed!"),
