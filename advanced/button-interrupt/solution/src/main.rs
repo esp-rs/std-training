d0a908e412c4ac4078bc33fd61c6da2f7d3eb0ac
Mirabellensaft <tanks@ferrous-systems.com>
2022-02-24 17:21:00 +0100

    add comments

--- advanced/button-interrupt/solution/src/main.rs
+++ advanced/button-interrupt/solution/src/main.rs
@@ -11,7 +11,6 @@ use esp_idf_sys::{
 };
 
 static mut EVENT_QUEUE: Option<QueueHandle_t> = None;
-static mut THING: u32 = 0;
 const PORT_MAX_DELAY: u32 = 0xffffffff;
 
 // TODO place code in ram using IRAM linker feature
@@ -24,7 +23,8 @@ unsafe extern "C" fn button_interrupt(_: *mut c_void) {
 
 fn main() -> anyhow::Result<()> {
     const GPIO_NUM: i32 = 9;
-
+    
+    // Configures the button
     let io_conf = gpio_config_t {
         pin_bit_mask: 1 << GPIO_NUM,
         mode: gpio_mode_t_GPIO_MODE_INPUT,
@@ -33,17 +33,25 @@ fn main() -> anyhow::Result<()> {
         intr_type: gpio_int_type_t_GPIO_INTR_POSEDGE, // positive edge trigger = button down
     };
 
+    // Queue configurations
     const QUEUE_TYPE_BASE: u8 = 0;
     const ITEM_SIZE: u32 = 0; // we're not posting any actual data, just notifying
     const QUEUE_SIZE: u32 = 2; // 1 might be enough?
 
     unsafe {
-        EVENT_QUEUE = Some(xQueueGenericCreate(QUEUE_SIZE, ITEM_SIZE, QUEUE_TYPE_BASE));
-
+        // Writes button configuration into register
         esp!(gpio_config(&io_conf))?;
-
+        
+        // Instantiates the queue
+        EVENT_QUEUE = Some(xQueueGenericCreate(QUEUE_SIZE, ITEM_SIZE, QUEUE_TYPE_BASE));
+        
+        // Sets the flag that the interrupt is handled in RAM
         const ESP_INTR_FLAG_IRAM: i32 = 1 << 10; // ISR will be executed even when caches are disabled
+        
+        // Installs the interrupt handler
         esp!(gpio_install_isr_service(ESP_INTR_FLAG_IRAM))?;
+
+        // Adds the GPIO to the interrupt handler and the function that is to be executed upon firing of the interrupt
         esp!(gpio_isr_handler_add(
             GPIO_NUM,
             Some(button_interrupt),
@@ -51,9 +59,11 @@ fn main() -> anyhow::Result<()> {
         ))?;
     }
 
+    // Reads the queue in a loop.
     loop {
         unsafe {
             let res = xQueueReceive(EVENT_QUEUE.unwrap(), ptr::null_mut(), PORT_MAX_DELAY);
+            // If the item in the queue is > 0, the button is pressed. 
             match res {
                 _ => println!("button pressed!"),
                 0 => {}
