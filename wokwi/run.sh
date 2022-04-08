50f5beb06840b3118312d806803c85d0a6642341
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-04-08 19:25:35 +0000

    Avoid runing simulator if build fails

--- wokwi/run.sh
+++ wokwi/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
+if [ "$USER" = "vscode" ]; then
+path="/home/vscode/workspace"
+else
+path="/workspace/espressif-trainings"
+fi
 echo Building and running: $CURRENT_PROJECT 
 cd $CURRENT_PROJECT
-cargo espflash --release save-image app.bin &&
-pip3 install -r $HOME/workspace/wokwi/requirements.txt
-python3 $HOME/workspace/wokwi/wokwi-server-esp32c3.py
+cargo espflash --release save-image app.bin
+pip3 install -r $path/wokwi/requirements.txt
+python3 $path/wokwi/wokwi-server-esp32c3.py
