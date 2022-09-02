f1561fdd1deb91b9a16c16f92d341215d7e2d756
aissata <aimaiga2@gmail.com>
2022-09-02 08:18:44 +0200

    Not working yet, typical output: I (5490) mqtt: Entry point :: BeforeConnect I (5490) mqtt: Some stuff: BeforeConnect I (6500) mqtt: Entry point :: Connected(false) I (6500) mqtt: True or false false I (7500) mqtt: Entry point :: Published(62839) I (7500) mqtt: MQTT Message : Published(62839) I (8500) mqtt: Entry point :: Published(18102)

--- intro/mqtt/solution/src/main.rs
+++ intro/mqtt/solution/src/main.rs
@@ -1,4 +1,4 @@
-use std::{convert::TryFrom, thread::sleep, time::Duration};
+use std::{convert::TryFrom, thread, thread::sleep, time::Duration};
 
 use bsc::{
     led::{RGB8, WS2812RMT},
@@ -6,7 +6,10 @@ use bsc::{
     wifi::wifi,
 };
 use embedded_svc::mqtt::client::{
-    Client, Details::Complete, Event::Received, Message, Publish, QoS, Connection, MessageImpl,
+    Client, Connection,
+    Details::Complete,
+    Event::{Connected, Published, Received},
+    Message, MessageImpl, Publish, QoS,
 };
 use esp32_c3_dkc02_bsc as bsc;
 use esp_idf_svc::{
@@ -16,13 +19,13 @@ use esp_idf_svc::{
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_sys as _;
 use log::{error, info};
-use mqtt_messages::{hello_topic, ColorData};
+use mqtt_messages::{hello_topic, temperature_data_topic, ColorData};
 
 const UUID: &'static str = get_uuid::uuid();
 
 #[toml_cfg::toml_config]
 pub struct Config {
-    #[default("10.1.1.199")]
+    #[default("localhost")]
     mqtt_host: &'static str,
     #[default("")]
     mqtt_user: &'static str,
@@ -61,18 +64,28 @@ fn main() -> anyhow::Result<()> {
     } else {
         format!("mqtt://{}", app_config.mqtt_host)
     };
-    let (mut client, mut connection) =
-    EspMqttClient::new_with_conn(broker_url, &mqtt_config)?;
+    let (mut client, mut connection) = EspMqttClient::new_with_conn(broker_url, &mqtt_config)?;
+    info!("MQTT client started.");
+    //info!("MQTT client: {:?}", &client);
+    //info!("MQTT connection: {:?}", &connection);
 
     let payload: &[u8] = &[];
-    client.publish(&hello_topic(UUID), QoS::AtLeastOnce, true, payload)?;
+    println!("MQTT Listening for messages");
 
-    client.subscribe(&mqtt_messages::color_topic(UUID), QoS::AtLeastOnce)?;
-
+    //thread::spawn(move || {
+    //client.subscribe(&mqtt_messages::color_topic(UUID), QoS::AtLeastOnce);
     loop {
         // TODO: switch to handling this in a callback or seperate thread
-        if let Some(Ok(Received(msg))) = connection.next() {
-            process_message(&msg, &mut led)
+        if let Some(Ok(r)) = connection.next() {
+            info!("Entry point :: {:?}", r);
+
+            match r {
+                Received(recieved_bytes) => info!("Some received bytes {:?}", recieved_bytes),
+                Connected(tof) => info!("True or false {:?}", tof),
+                Published(message_id) => info!("MQTT Message : Published({})", message_id),
+                _ => info!("Some stuff: {:?}", r),
+            }
+            //process_message(&msg, &mut led);
         }
         sleep(Duration::from_secs(1));
         let temp = temp_sensor.read_owning_peripherals();
@@ -81,13 +94,18 @@ fn main() -> anyhow::Result<()> {
             QoS::AtLeastOnce,
             false,
             &temp.to_be_bytes() as &[u8],
-        )?;
+        );
     }
+
+    println!("MQTT connection loop exit");
+    //});
+
+    Ok(())
 }
 
 fn process_message(message: &MessageImpl, led: &mut WS2812RMT) {
     match message.details() {
-        Complete =>  {
+        Complete => {
             info!("{:?}", message);
             let message_data: &[u8] = message.data();
             if let Ok(ColorData::BoardLed(color)) = ColorData::try_from(message_data) {
@@ -96,7 +114,7 @@ fn process_message(message: &MessageImpl, led: &mut WS2812RMT) {
                     error!("could not set board LED: {:?}", e)
                 };
             }
-        },
+        }
         _ => error!("could not set board LED"),
     }
 }
