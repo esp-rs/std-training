f4704d59b08324870838973646d16637c830de79
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-02-24 12:43:34 +0100

    feat: ✨ Add examples

--- .devcontainer/test.sh
+++ .devcontainer/test.sh
@@ -9,3 +9,27 @@ echo "Compiling $1"
 cd /workspace/$1
 $HOME/.cargo/bin/cargo clean
 $HOME/.cargo/bin/cargo build
+
+if [[ "$1" == advanced/button-interrupt ]]; then
+    $HOME/.cargo/bin/cargo build --example solution
+    $HOME/.cargo/bin/cargo build --example solution_led
+fi
+
+if [[ "$1" == advanced/i2c-sensor-reading ]]; then
+    $HOME/.cargo/bin/cargo build --example part_1
+    $HOME/.cargo/bin/cargo build --example part_2
+fi
+
+if [[ "$1" == intro/http-client ]]; then
+    $HOME/.cargo/bin/cargo build --example http_client
+    # $HOME/.cargo/bin/cargo build --example https_client
+fi
+
+if [[ "$1" == intro/http-server ]]; then
+    $HOME/.cargo/bin/cargo build --example http_server
+fi
+
+if [[ "$1" == intro/mqtt/exercise ]]; then
+    $HOME/.cargo/bin/cargo build --example solution_publ_rcv
+    $HOME/.cargo/bin/cargo build --example solution_publ
+fi
