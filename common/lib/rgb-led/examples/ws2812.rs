68a62e7643e6f6cdf300d6fa490e55e7f5e6521e
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-03-13 12:48:03 +0100

    feat: ✨ Add rgb-led crate

---
+++ common/lib/rgb-led/examples/ws2812.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::peripherals::Peripherals;
+use log::info;
+use rgb_led::WS2812RMT;
+// If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp_idf_sys as _;
+
+fn main() -> Result<()> {
+    esp_idf_sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().unwrap();
+    // Onboard RGB LED pin
+    // Rust ESP Board gpio2,  ESP32-C3-DevKitC-02 gpio8
+    let led = peripherals.pins.gpio2;
+    let channel = peripherals.rmt.channel0;
+    let mut ws2812 = WS2812RMT::new(led, channel)?;
+    loop {
+        info!("Red!");
+        ws2812.set_pixel(rgb::RGB8::new(255, 0, 0))?;
+        FreeRtos::delay_ms(1000);
+        info!("Green!");
+        ws2812.set_pixel(rgb::RGB8::new(0, 255, 0))?;
+        FreeRtos::delay_ms(1000);
+        info!("Blue!");
+        ws2812.set_pixel(rgb::RGB8::new(0, 0, 255))?;
+        FreeRtos::delay_ms(1000);
+    }
+}
---
+++ common/lib/rgb-led/src/lib.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use core::time::Duration;
+use esp_idf_hal::{
+    gpio,
+    rmt::{config::TransmitConfig, FixedLengthSignal, PinState, Pulse, TxRmtDriver, CHANNEL0},
+};
+
+pub use rgb::RGB8;
+
+pub struct WS2812RMT<'a> {
+    tx_rtm_driver: TxRmtDriver<'a>,
+}
+
+impl WS2812RMT<'_> {
+    // Rust ESP Board gpio2,  ESP32-C3-DevKitC-02 gpio8
+    pub fn new(led: gpio::Gpio2, channel: CHANNEL0) -> Result<Self> {
+        let config = TransmitConfig::new().clock_divider(2);
+        let tx = TxRmtDriver::new(channel, led, &config)?;
+        Ok(Self { tx_rtm_driver: tx })
+    }
+
+    pub fn set_pixel(&mut self, rgb: RGB8) -> Result<()> {
+        let color: u32 = ((rgb.g as u32) << 16) | ((rgb.r as u32) << 8) | rgb.b as u32;
+        let ticks_hz = self.tx_rtm_driver.counter_clock()?;
+        let t0h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(350))?;
+        let t0l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(800))?;
+        let t1h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(700))?;
+        let t1l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(600))?;
+        let mut signal = FixedLengthSignal::<24>::new();
+        for i in (0..24).rev() {
+            let p = 2_u32.pow(i);
+            let bit = p & color != 0;
+            let (high_pulse, low_pulse) = if bit { (t1h, t1l) } else { (t0h, t0l) };
+            signal.set(23 - i as usize, &(high_pulse, low_pulse))?;
+        }
+        self.tx_rtm_driver.start_blocking(&signal)?;
+
+        Ok(())
+    }
+}
+
+fn ns(nanos: u64) -> Duration {
+    Duration::from_nanos(nanos)
+}
